2177bd754c907f291d25409ff702d0c53e79cea3
Lionel <Lionel@DESKTOP-LPM0193>
2017-11-25 13:00:11 +0200

    Migrations and form validations

--- AwesomeUniversity/Models/Student.cs
+++ AwesomeUniversity/Models/Student.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AwesomeUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; } // this means that each student has a list of enrollments associated with him
